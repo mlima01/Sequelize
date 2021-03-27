c8ce11a5e00476f6f572218e55992e6d9e2329c1
mlima <mlima@umd.edu>
2021-03-26 20:48:20 -0400

    Update script.js

---
+++ public/script.js
@@ -0,0 +1,11 @@
+//In your front-end JS scripts, 
+//do a fetch request using async/await 
+//to your server that will return all dining 
+//halls on campus.
+//The API for this already 
+//exists in the new repository.
+
+async function windowActions(){
+    const endpoint
+}
+window.onload = windowActions;
