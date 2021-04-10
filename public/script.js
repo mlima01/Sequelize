bdf413e144963709a92457c19c71b037732bcb59
mlima <mlima@umd.edu>
2021-04-10 12:10:34 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,3 +1,10 @@
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+  }
+
+
 async function getMeals(){
     const endpoint = '/api/wholeMeals';
     const request = await fetch(endpoint);
