afa60f3eb4eda9a6ef75eda291fa860af69b262a
mlima <mlima@umd.edu>
2021-04-09 23:25:43 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,35 +1,37 @@
-//In your front-end JS scripts, 
-//do a fetch request using async/await 
-//to your server that will return all dining 
-//halls on campus.
-//The API for this already 
-//exists in the new repository.
-
-async function windowActions(){
-    const endpoint = '/api/dining';
+async function getMeals(){
+    const endpoint = '/api/meals';
     const request = await fetch(endpoint);
     const data = await request.json();
-    
+    return data
+}
 
-    for (item in data) {
-        dataArr = data[item];
-        //console.log(dataArr)
-        for (i in dataArr){
+
+async function windowActions(){
+    console.log("loaded window")
+    const meals = await getMeals();
+    console.table(meals);
+
+
+    // for (item in data) {
+    //     dataArr = data[item];
+    //     console.log(dataArr)
+    //     for (i in dataArr){
             
-            row.innerHTML +=
-                    `<tr>
-                    <th>${dataArr[i].hall_id}</th>
-                    <th>${dataArr[i].hall_name}</th>
-                    <th>${dataArr[i].hall_address}</th>
-                    </tr>`};
+    //         row.innerHTML +=
+    //                 `<tr>
+    //                 <th>${dataArr[i].meal_id}</th>
+    //                 <th>${dataArr[i].meal_name}</th>
+    //                 <th>${dataArr[i].meal_category}</th>
+    //                 </tr>`};
             
 
-        }}
+    //     }}
+}
         
     
      
     
         
-    const row = document.querySelector("tbody")
+    //const row = document.querySelector("tbody")
     
 window.onload = windowActions;
